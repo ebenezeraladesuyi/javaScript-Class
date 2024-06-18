e217b2ab2bc0e031a6aa60ff9ef2da6aaabd1c4d
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-06-18 13:04:36 +0100

    new

---
+++ dataTypes.js
@@ -0,0 +1,67 @@
+// Primitive : string ; number ; int ; null ; bigit ; boolean ; undefined 
+// Non-Primitive : object (object ; array )
+
+// var ; let ; const
+
+// variable
+// string = ""
+var first = 'welcome' 
+// var = keyword ; first = variable name/declaration ; "welcome" : string = variabe value
+var first = "good"
+// console.log(first)
+
+// number = 000
+let dollars = 1500
+dollars = 1405
+dollars = 1320
+let dollar2 = dollars
+// console.log(dollar2)
+
+// boolean = true/false
+const light = true
+const light2  = light
+const greet = "Good morning"
+// console.log(greet)
+
+// null
+const greet2 = null
+// console.log(greet2)
+
+
+// object : object & array
+
+// object = {key : value}
+const boy = {"maths": 20}
+
+// Json data []
+const jss1 = [
+    {"Bola": "female"},
+    {"Kola" : "male"},
+    {"Jane" : "female"}
+]
+// console.log(jss1)
+
+// arrray
+const allData = [20, "boy", {"age" : 15}, undefined, null, [20, "boy", {"age" : 15}, undefined, null], "girl"]
+
+const facebookUsers = [
+        {
+        "name" : "Ballar Skibby",
+        "Location" : "California, Ibadan",
+        "Age" : 18,
+        "email" : "ballar@test.com"
+        }, 
+        {
+        "name" : "Joe Pry",
+        "Location" : "California, Ibadan",
+        "Age" : 28,
+        "email" : "joe@test.com"
+        }, 
+        {
+        "name" : "Gift Eboyi",
+        "Location" : "California, Ibadan",
+        "Age" : 19,
+        "email" : "gift@test.com"
+        },   
+]
+console.log(facebookUsers)
---
+++ jsFirstClass.js
@@ -0,0 +1,67 @@
+// Datatypes
+// Primitive and Non-primitive datatypes
+
+// Primitive : string, number, boolean, bigInt, undefined, null
+
+// Non-Primitive : Object [array, objects, functions]
+
+// "var", "let" and "const"
+
+// var
+var greet = "Good morning" 
+// console.log(greet)
+// console.log(typeof greet)
+
+greet = "morning"
+console.log(greet)
+
+// // let
+// let seven = 15
+// console.log(seven)
+// console.log(typeof seven)
+
+// let sevven = 10
+// let sevenn = 28
+// console.log(sevven + sevenn)
+
+// // const
+const greeting = "hello"
+console.log(greeting)
+
+// const greetings = "world"
+// console.log(greetings)
+
+// boolean
+const decide = true
+// const decid = false
+console.log(typeof decide)
+console.log(decide)
+// console.log(typeof decide)
+// console.log(typeof decid)
+
+const come = undefined
+console.log(typeof come)
+
+
+// // Non-primitives
+// objects = {key :value}
+
+const student = {name: "Kola", class: "sss3", department: "science", subject: "mathematics", score: 65}
+// console.log(student)
+// console.log(typeof student)
+
+// // array
+
+const allData = [100, "kola", undefined, true, {name: "Joy", score: 70}, "money"]
+// console.log(allData)
+
+const students = [
+    kola = {name: `${students.id_}`, class: "sss3", department: "science", subject: "mathematics", score: 65},
+    Ade = {name: "Ade", class: "sss3", department: "science", subject: "mathematics", score: 62},
+    Musa = {name: "Musa", class: "sss3", department: "science", subject: "mathematics", score: 75},
+]
+console.log(students)
+// // console.log(typeof students)
+
+// const person = "Jane"
+
